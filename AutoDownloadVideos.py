1aa589ca5e4dda779ba214b8e43151a7f17fbe6c
iceboy95 <hqkhor95@gmail.com>
2016-11-01 13:30:52 +0800

    downloader done

---
+++ AutoDownloadVideos.py
@@ -0,0 +1,81 @@
+import urllib.request as urlRequest
+# Fixed Variables
+counter = 0
+dayArray = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17'
+,'18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+monthArray = ['01','02','03','04','05','06','07','08','09','10','11','12',]
+# Tunable Variables
+currentDay = dayArray[0]
+currentMonth = monthArray[0]
+currentYear = 2013
+pointerDay = 0
+pointerMonth = 0
+# login credentials
+username = 'collaborator'
+password = 'bandwidth'
+
+
+while counter < 100:
+	# http://lost.cse.wustl.edu/static/camera/001/001_2013-01-01_11-00-01/001_2013-01-01_11-00-01.avi
+	#template url: http://lost.cse.wustl.edu/static/camera/001/001_2013-08-07_11-00-01/001_2013-08-07_11-00-01.avi
+	# getting the complete url ready
+	manager = urlRequest.HTTPPasswordMgrWithPriorAuth()
+	try:
+		filename = '001_' + str(currentYear) + currentMonth + currentDay + '.avi'
+		url = 'http://lost.cse.wustl.edu/static/camera/001/001_' + str(currentYear) + '-' + currentMonth + '-' + currentDay + '_11-00-01/001_' + str(currentYear) + '-' + currentMonth + '-' + currentDay + '_11-00-01.avi'
+		manager.add_password(None, url, username, password)
+		auth = urlRequest.HTTPBasicAuthHandler(manager)
+		opener = urlRequest.build_opener(auth)
+		urlRequest.install_opener(opener)
+		#Scrape it!
+		print(url)
+		content = urlRequest.urlretrieve(url, filename)
+	except:
+		pass
+	counter += 1
+
+	if (pointerMonth == 3) or (pointerMonth == 5) or (pointerMonth == 8) or (pointerMonth == 10):
+		if currentDay == '30':
+			currentDay = dayArray[0]
+			pointerMonth += 1
+			currentMonth = monthArray[pointerMonth]
+			pointerDay = 0			
+		else:
+			pointerDay += 1
+			currentDay = dayArray[pointerDay]
+			
+	elif pointerMonth == 1:
+		if currentYear%4 == 0 and currentDay == '28':#leapyear
+			pointerDay += 1
+			currentDay = dayArray[pointerDay]
+		elif currentYear%4 == 0 and currentDay == '29':#reset to 01 if day ends for the month
+			currentDay = dayArray[0]
+			pointerDay = 0
+			pointerMonth += 1
+			currentMonth = monthArray[pointerMonth]
+		else:
+			currentDay = dayArray[0]
+			pointerDay = 0
+			pointerMonth += 1
+			currentMonth = monthArray[pointerMonth]
+	else: #for 31 days month
+		if currentDay == '30':
+			pointerDay += 1
+			currentDay = dayArray[pointerDay]
+		elif currentDay == '31':
+			currentDay = dayArray[0]
+			pointerDay = 0
+			pointerMonth += 1
+			currentMonth = monthArray[pointerMonth]
+		else:
+			pointerDay += 1
+			currentDay = dayArray[pointerDay]
+
+	
+	#if year ends?
+	if (pointerMonth == 11) and (pointerDay == 30):
+		currentYear += 1
+		pointerMonth = 0
+		pointerDay = 0
+		currentMonth = monthArray[pointerMonth]
+		currentDay = dayArray[pointerDay]
